284add05a0b866ae76c8280d705a84185907ae69
netajik <kancharapunetaji@gmail.com>
2023-11-12 11:02:04 +0530

    initial commit

---
+++ src/github.com/netajik/App/Main.go
@@ -0,0 +1,17 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	var i = 12
+	j := 13
+	fmt.Printf("%v, %T", i, i)
+	fmt.Printf("%v, %T", j, j)
+	test()
+}
+
+func test() {
+	fmt.Println(" World")
+}
