2aa3e8741d79bf9af69a9d096851c0aba203448c
netajik <kancharapunetaji@gmail.com>
2023-11-24 19:32:12 +0530

    login with token, visit home page with toke in cookie and refresh token handlers implemented

--- src/github.com/netajik/App/Main.go
+++ src/github.com/netajik/App/Main.go
@@ -9,6 +9,11 @@ import (
 
 var i = 14
 
+//Foo struct
+type Foo struct {
+	bar int
+}
+
 func main() {
 	//variables
 	//var i = 12
@@ -20,13 +25,31 @@ func main() {
 	fmt.Printf(" %v, %T\n", k, k)
 
 	//data types
-	var foo bool = false
-	fmt.Println(foo)
+	var fo bool = false
+	fmt.Println(fo)
 	var a float32 = 4;
 	fmt.Println(a)
 
 	//functions
 	//test()
+
+	//defer
+	fmt.Println("defer")
+	fmt.Println(1)
+	//defer fmt.Println(2)
+	fmt.Println(3)
+
+	//pointers used to sotore address of a location
+	fmt.Println("\npointers")
+	var foo *Foo
+	fmt.Println(foo)
+	foo= new(Foo) // assigning memory for foo
+	fmt.Println(foo) // reference
+	fmt.Println((*foo)) //dereference
+	fmt.Println((*foo).bar)
+
+	//or compiler dereference during compile time
+	fmt.Println(foo.bar)
 }
 
 func test() {
--- src/github.com/netajik/go-jwt/main.go
+++ src/github.com/netajik/go-jwt/main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	http.HandleFunc("/login",Login)
-	http.HandleFunc("/login",Home)
-	http.HandleFunc("/login",Refresh)
+	http.HandleFunc("/home",Home)
+	http.HandleFunc("/refresh",Refresh)
 
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
